38db2e11ef80d6eb215fc0f8555b9b7488dd316f
TANYA9103 <96692584+TANYA9103@users.noreply.github.com>
2022-09-09 15:07:29 +0530

    New Sorting

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,21 +1,40 @@
 
 public class BubbleSort {
-	static void bubbleSort(int[] arr) 
-	{  
-        int n = arr.length;  
-        int temp = 0;  
-        for(int i=0; i < n; i++)
-        {  
-        	for(int j=1; j < (n-i); j++)
-        	{  
-        		if(arr[j-1] > arr[j])
-        		{  
-        			temp = arr[j-1];  
-        			arr[j-1] = arr[j];  
-        			arr[j] = temp;  
-        		}  
-                          
-        	}  
-         }  
-	}
-}
+	 static void print (int a[]) //function to print array elements  
+	    {  
+	        int n = a.length;  
+	        int i;  
+	        for (i = 0; i < n; i++)  
+	        {  
+	            System.out.print(a[i] + " ");  
+	        }         
+	    }  
+	    static void bubbleSort (int a[])    // function to implement bubble sort  
+	    {  
+	        int n = a.length;  
+	        int i, j, temp;  
+	        for (i = 0; i < n; i++)  
+	        {  
+	            for (j = i + 1; j < n; j++)  
+	            {  
+	                if (a[j] < a[i])  
+	                {  
+	                    temp = a[i];  
+	                    a[i] = a[j];  
+	                    a[j] = temp;  
+	                }  
+	            }  
+	        }  
+	    }  
+	    public static void main(String[] args) {    
+	    int a[] = {35, 10, 31, 11, 26};    
+	    BubbleSort b1 = new BubbleSort();  
+	    System.out.println("Before sorting array elements are - ");    
+	    b1.print(a);  
+	    b1.bubbleSort(a);  
+	    System.out.println();  
+	    System.out.println("After sorting array elements are - ");    
+	    b1.print(a);  
+	        
+	}    
+	}  
